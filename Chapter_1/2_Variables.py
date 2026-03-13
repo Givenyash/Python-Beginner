aceacffa650ac00bb7f257e363fc9f6a78406227
Givenyash <yash.ishu.barehi@gmail.com>
2026-03-13 21:57:22 +0530

    Variables in python

---
+++ Chapter_1/2_Variables.py
@@ -0,0 +1,21 @@
+#Python itself recognize the datatype of the variable 
+#We do not need to initialize its dataType
+
+name = "Yash gautam"
+age1 = 20
+age2 = 31
+
+print("Actual Value :",age2)
+
+favsubject ="Maths"
+age2=age1
+#name of variable do not contain any special character and numbers in the starting
+
+print("Changed Value :" , age2)
+#some python keywords are also not allowed as they are reserved
+#for example , if , for , while , etc
+
+#user input 
+
+name = input("Enter your name : ")
+print("Hello !" , name)
