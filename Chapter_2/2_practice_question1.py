c1fa67cfebb4b476296d59d36b0931e5c86c7c98
Givenyash <yash.ishu.barehi@gmail.com>
2026-03-15 20:36:05 +0530

    practice question no. 1

---
+++ Chapter_2/2_practice_question1.py
@@ -0,0 +1,6 @@
+# write a python program that takes your age as input and prints :
+# the value entered
+# its data type
+
+age = int(input("Enter age :" ))
+print("The value entered:",age, "and its DataType is :", type(age))
