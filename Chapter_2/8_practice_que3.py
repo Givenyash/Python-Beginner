d09ee0058f475a1d798f20aa244a7d072f498b72
Givenyash <yash.ishu.barehi@gmail.com>
2026-03-15 20:38:57 +0530

    practice que no. 3

---
+++ Chapter_2/8_practice_que3.py
@@ -0,0 +1,12 @@
+# write a program that takes two numbers and prints :
+#     Their sum, difference , and product.
+#     Whether the first number is greater than the second
+
+a = int(input("Enter a :"))
+b = int (input("Enter b :"))
+
+print("sum is :", a + b)
+print("difference is :", a-b)
+print("product is :", a*b)
+print() #single line space 
+print("a is greater ? ", a>b)
