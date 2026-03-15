cd792bae6aae56ff9a7efead6a00f87a4eb089b9
Givenyash <yash.ishu.barehi@gmail.com>
2026-03-15 20:35:39 +0530

    data types in python

---
+++ Chapter_2/1_dataTypes.py
@@ -0,0 +1,8 @@
+name =("Yash")
+age = 21
+area = 3.14
+food = "Samosa"
+
+print("DataType of of the variable name : ",type(name))
+print("DataType of of the variable age : ",type(age))
+print("DataType of of the variable area : ",type(area))
