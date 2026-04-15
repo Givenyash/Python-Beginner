9879811574537cfc0206c75a10cf7fa9b3d0e48a
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-15 16:38:17 +0530

    Complete operators in python

--- Chapter_2/7_operators_in_python.py
+++ Chapter_2/7_operators_in_python.py
@@ -1,65 +1,59 @@
 # Operators perform operations on variable and values.
 # types of operator : Arithmetic , Comparison , Logical, Assignment, Bitwise
+# Python follows BODMAS Rule
 
-#Example
-x = 5
-y = 3
-print(x + y)
-print( x > y)
-print ( x > 0 and y < 20)
-
-#Arithmatic operators
+#Arithmatic operators : perform mathematical operations
 print()
 print("Arithmatic operators")
 x=10
-y=6
+y=5
 
-print("Sum:",x+y)
-print("Difference:",x-y)
-print("Product:",x*y)
-print("Divison:",x/y)
-print("Modulus:",x%y)
-print("Power:", x**y)
+print("Sum:", x + y) # (+)
+print("Difference:", x - y) # (-)
+print("Product:", x * y) # (*)
+print("Divison:", x / y) # (/)
+print("Modulus:", x % y) # (%)
+print("Power:", x ** y) # (**)
+
+print(x / y) # output => 2.0
+print(x // y) # output => 2 , this is called floor division , it results in integer.
 
 print()
+
 
 print("Comparison operators")
-#Comparison operators
+#Comparison operators : These are also called relational operators and are used to compare two values. This will always provide Boolean result. A comparison btw a string and a number can't possible.
 
-print("Equality:",x==y)
-print("Greater than or equal to :",x>=y)
-print("Less than or equal to :",x<=y)
-print("x is greater than y :",x>y)
-print("y is greater than x :",x<y)
-print("x is not equal to y :",x!=y)
+print("Equality:",x == y) # (==)
+print("Greater than or equal to :", x >= y) # (>=)
+print("Less than or equal to :", x <= y) # (<=)
+print("x is greater than y :", x > y) # (>)
+print("y is greater than x :", x < y) # (<)
+print("x is not equal to y :", x != y) # (!=)
+# They will work with numsbers but we can use them with strings as well. Strings will be compared through their ASCII values.
 
 print()
+
 
 print("Logical operator")
+#Logical operator : These are used in Python to combine multiple conditions and return a Boolean result.
 
-#Logical operator
-
-print("AND operator :",x>y and y<10)
-print("OR operator :" , x<y or y < 10)
-print("NOT operator :",not(x>y))
+print("AND operator :",x > y and y < 10) # return true if both conditions are true, else false
+print("OR operator :" , x < y or y < 10) # return true when one or more of the condition is true
+print("NOT operator :",not(x > y)) # reverse the boolean value
 
 print()
 
+
 print("Assignment operator")
+#Assignment operator : These are the operators used to assign values to variables.
 
-#Assignment operator
-
-a = 2
-b = 3
-
-a=a+6
-a+=6
-
-a=a-1
-a-=1
-
-a=a/10
-a/=10
-
-a=a*10
-a*=10
+a = 2 # just assign
+a+=6 # Add & assign
+a-=1 # Substract & assign
+a/=10 # Divide and assign
+a*=10 # Multiply and assign
+a%=2 # modulus and assign  
+a//=10 # Floor divide and assign
+a**= 5 # Exponentiation and assign
+print(a)
