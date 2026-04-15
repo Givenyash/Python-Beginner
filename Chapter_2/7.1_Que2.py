871c2e015f707a54350ef40a10d9f4ea20d76343
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-15 16:38:37 +0530

    Questions related to logical operators

---
+++ Chapter_2/7.1_Que2.py
@@ -0,0 +1,13 @@
+# Answer True or False
+
+#Print (126 > 130)
+print(126 > 130)
+
+#Print ((456 == 456) != (235 == 236))
+print((456 == 456) != (235 == 236))
+
+#print (12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
+print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
+
+#print(True and bool(0))
+print(True and bool(0))
