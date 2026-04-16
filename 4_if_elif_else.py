37167e6a1e16da7001b3b7371410fc6e1c9b2049
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-16 22:40:41 +0530

    if-elif-else

---
+++ 4_if_elif_else.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your number :"))
+
+if a == 10: 
+    print("I will do Task A") #True
+
+elif 100<=a<=200:
+    print("This is correct statement of if-elif-else.") #True agar first condition fail hui toh
+
+else: 
+    print("I will not do task ") #False
+
+# This is called as if-else ladder or nested if-else.
