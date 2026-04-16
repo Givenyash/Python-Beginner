f919fdaaaaaef362bef330e2df2466883efe4997
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-16 22:46:06 +0530

    Moved to a new folder

--- 1_conditional_statements.py
+++
@@ -1,9 +0,0 @@
-print("Conditional Statements : (if , if-else , if-elif-else)")
-
-#Conditional statements in python allow decision - making by executing different blocks of code based on conditions.
-
-#There are 3 types of conditional statements
-
-#if statement  :- Executes if the condition is True.
-#if - else statement  :- Executes if True, another if False.
-#if-elif-else  :- Checks multiple condition in sequence.
