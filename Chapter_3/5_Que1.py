cc87257d81dae56635e64283a4226b1ce032eaf4
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-16 22:51:46 +0530

    Comparison btw 2 numbers

---
+++ Chapter_3/5_Que1.py
@@ -0,0 +1,10 @@
+# Accept two numbers and print the greatest between them.
+
+a = int(input("Enter first number :")) 
+b = int(input("Enter another number :"))
+
+if a > b:
+    print("A is greater than B.")
+
+else:
+    print("B is greater than A.")
