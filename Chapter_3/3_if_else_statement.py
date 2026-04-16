3936b39a8f1c8c0b362246d107e676d15f10ac81
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-16 22:47:02 +0530

    if-else statements.

---
+++ Chapter_3/3_if_else_statement.py
@@ -0,0 +1,8 @@
+# This is called if-else statements
+a = int(input("Enter your number :"))
+
+if a >= 10: # it runs the block of code if the condition is true  
+    print("I will do Task A") #True
+
+else: # if condition is false then else statement automatically runs the else statement.
+    print("I will not do task ") #False
