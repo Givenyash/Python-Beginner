5a4b1d58c0799fd7fb85791f5ced47fb48c56f16
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-16 22:44:53 +0530

    if-elif-else statement.

--- 4_if_elif_else.py
+++ 4_if_elif_else.py
@@ -1,12 +1,13 @@
-a = int(input("Enter your number :"))
+money = int(input("Please give me money :"))
 
-if a == 10: 
-    print("I will do Task A") #True
+if 0<=money<100: 
+    print("I will have a choco bar ice-cream.") #True
 
-elif 100<=a<=200:
-    print("This is correct statement of if-elif-else.") #True agar first condition fail hui toh
+elif 100<=money<=200:
+    print("I can buy now more than 10 ice-creams.") #True agar first condition fail hui toh
 
 else: 
-    print("I will not do task ") #False
+    print("I can't handle handle the money. ") #False
 
 # This is called as if-else ladder or nested if-else.
+# elif checks conditions in sequence.
