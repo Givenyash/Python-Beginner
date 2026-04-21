d1ff9850d141430a1bc96d46717a30e8cfa9d7fb
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-21 23:20:34 +0530

    Reversal of a string

---
+++ Chapter_4/15_reverse_string.py
@@ -0,0 +1,14 @@
+#Reverse a string without using in build functions...
+
+s = "SUNSTONE"
+
+for i in range( len(s) - 1, 0-1, -1):       #Size of string is 8 here and indexing starts from 0 in strings
+
+    print(s[i])
+
+#String concatination
+a = "SUNSTONE"
+
+b = "LEARINING IN "
+
+print(b + a)
