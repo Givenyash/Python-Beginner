34b12d1f2249967e52af9ae199d49a72903507f3
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-21 23:00:59 +0530

    check a number wheather a perfect number or not

---
+++ Chapter_4/13_check_a_number_perfect_or_not.py
@@ -0,0 +1,11 @@
+#Accept a number and check if it a perfect number or not...
+#Perfect number :- a number whose sum of factors is equal to the number itself...
+
+num = int(input("Insert a number you want to check :"))
+
+sum = 0
+for i in range(1, num+1 , 1):
+    if num % i == 0:
+        sum = sum + i
+        if sum == num:
+            print(f"The given number {num} is a perfect number") 
