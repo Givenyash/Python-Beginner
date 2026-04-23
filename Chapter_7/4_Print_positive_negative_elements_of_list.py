bdc1c1ae7202dee795cfeba5b464278c8b22251c
Givenyash <yash.ishu.barehi@gmail.com>
2026-04-23 18:20:01 +0530

    print +ve and -ve elements of the List

---
+++ Chapter_7/4_Print_positive_negative_elements_of_list.py
@@ -0,0 +1,11 @@
+l = [-45, 67, 12, -68, -69, 34]
+
+print("Positive elements are ")
+for i in l:
+    if i >= 0:
+        print(i)
+
+print("Negative elements are ")
+for i in l:
+    if i < 0:
+        print(i)
